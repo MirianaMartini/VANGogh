d7daa43c7bcd6ed5e4a6f6ef2c6b1df36bf6c4f9
renzocasarone <casaronerenzo@gmail.com>
2022-01-08 16:16:00 +0100

    Fix Van
    Sistemato ingresso/uscita FPV dal Van

--- Assets/VanRenzo/Script/VanEnterExitSystem.cs
+++ Assets/VanRenzo/Script/VanEnterExitSystem.cs
@@ -6,12 +6,16 @@ public class VanEnterExitSystem : MonoBehaviour
 {
     public GameObject VanCamera;
     public GameObject FPVCamera;
+    public Transform Van;
+    public Transform FPV;
+    public MonoBehaviour GuidaVanScript;
 
     // Start is called before the first frame update
     void Start()
     {
         FPVCamera.gameObject.active = true;
         VanCamera.gameObject.active = false;
+        GuidaVanScript.enabled = false;
     }
 
     // Update is called once per frame
@@ -26,17 +30,26 @@ public class VanEnterExitSystem : MonoBehaviour
     //Funzione che cambia camera
     public void cambioCamera()
     {
-        if (FPVCamera.gameObject.active)
+        if (FPVCamera.gameObject.active) //Guida VAN
         {
-            //imporre condizione che sia vicino a van o fare tramite raycasting
+            
+            //imporre condizione che sia vicino a van o fare tramite raycasting 
             FPVCamera.gameObject.active = false;
             VanCamera.gameObject.active = true;
+            GuidaVanScript.enabled = true;
+            //Metto controller sul Van
+            FPV.transform.SetParent(Van);
+            
+
         }
-        else if (VanCamera.gameObject.active)
+        else if (VanCamera.gameObject.active) //Muove personaggio
         {
-            //TODO farlo spawnare a sx del VAN
             FPVCamera.gameObject.active = true;
             VanCamera.gameObject.active = false;
+            //Disattivo script che permette di guidare il van
+            GuidaVanScript.enabled = false;
+            //Spawnare a sx del VAN
+            FPV.transform.SetParent(null);            
         }
     }
 }
