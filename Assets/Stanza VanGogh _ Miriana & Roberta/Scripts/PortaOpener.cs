7da32324cac81caa66acf0dc0a0c17bab6b07f72
MirianaMartini <mirimar0301@gmail.com>
2022-01-22 01:23:17 +0100

    RayTracing sulle porte e sul portone, cambiamento del FPSController (da aggiungere audio passi)

--- Assets/Stanza VanGogh _ Miriana & Roberta/Scripts/PortaOpener.cs
+++ Assets/Stanza VanGogh _ Miriana & Roberta/Scripts/PortaOpener.cs
@@ -2,26 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortaOpener : MonoBehaviour
+public class PortaOpener : Interactable
 {
     private Animator _animator;
     private bool _open = false;
-    
+
     void Start()
     {
         _animator = GetComponent<Animator>();
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-            if (_open == false)
-                Open();
-            else 
-                Close();
+    public override void Interact(GameObject caller) {
+        Debug.Log("Sto aprendo...");
+        if (_open == false)
+            Open();
+        else
+            Close();
     }
 
-    public void Open() {
+    public void Open()
+    {
+        Debug.Log("siamo nell'Open");
+        Debug.Log(_animator);
         if (_animator == null)
             return;
 
@@ -38,7 +40,41 @@ public class PortaOpener : MonoBehaviour
         _animator.SetBool("open", _open);
     }
 
+    /*
+       private Animator _animator;
+       private bool _open = false;
 
 
+       void Start()
+       {
+           _animator = GetComponent<Animator>();
+       }
+
+       void Update()
+       {
+           if (Input.GetKeyDown(KeyCode.E))
+               if (_open == false)
+                   Open();
+               else 
+                   Close();
+       }
+
+       public void Open() {
+           if (_animator == null)
+               return;
+
+           _open = true;
+           _animator.SetBool("open", _open);
+       }
+
+       public void Close()
+       {
+           if (_animator == null)
+               return;
+
+           _open = false;
+           _animator.SetBool("open", _open);
+       }
+       */
 
 }
