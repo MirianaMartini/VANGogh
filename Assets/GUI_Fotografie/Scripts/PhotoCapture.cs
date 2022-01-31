fa74b4a513c0fa6b1328c1b388c70ba62271845f
MirianaMartini <mirimar0301@gmail.com>
2022-01-31 22:14:57 +0100

    Inserimento Polaroid Inventory

--- Assets/GUI_Fotografie/Scripts/PhotoCapture.cs
+++ Assets/GUI_Fotografie/Scripts/PhotoCapture.cs
@@ -25,6 +25,7 @@ public class PhotoCapture : MonoBehaviour
 
     private Texture2D screenCapture;
     private bool viewingPhoto;
+    private Item polaroid;
 
     private void Start() 
     {
@@ -82,6 +83,11 @@ public class PhotoCapture : MonoBehaviour
     {
         Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
         photoDisplayArea.sprite = photoSprite;
+
+        polaroid = new Item();
+        polaroid.itemName = "Polaroid";
+        polaroid.icon = photoSprite;
+        InventoryManager.Instance.Add(polaroid);
 
         photoFrame.SetActive(true);
         
