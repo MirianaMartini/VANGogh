f20cd630cc0ead81598faea17b1d68b0bea00e4c
renzocasarone <casaronerenzo@gmail.com>
2022-02-07 21:51:07 +0100

    Fix script girasoli

--- Assets/TestRandomGirasoli/RandomPosition.cs
+++ Assets/TestRandomGirasoli/RandomPosition.cs
@@ -7,26 +7,17 @@ public class RandomPosition : MonoBehaviour
     public int numeroGirasoli;
     public int scalaGirasoli;
     public int rotazioneGirasoli;
-    public Terrain terrain;
+    public Terrain[] terrains;
     private int terrainWidth; // terrain size (x) 
     private int terrainLength; // terrain size (z) 
     private int terrainPosX; // terrain position x 
     private int terrainPosZ; // terrain position z 
 
     public GameObject[] girasoli;
-    private string[] arrayGirasoli = {"girasole1", "girasole9"}; 
 
     // Start is called before the first frame update
     void Start()
     {
-        // terrain size x 
-        terrainWidth = (int)terrain.terrainData.size.x;
-        // terrain size z 
-        terrainLength = (int)terrain.terrainData.size.z;
-        // terrain x position 
-        terrainPosX = (int)terrain.transform.position.x;
-        // terrain z position 
-        terrainPosZ = (int)terrain.transform.position.z;
         StartCoroutine(spawn());
     }
     
@@ -34,6 +25,18 @@ public class RandomPosition : MonoBehaviour
     {
         for (var i = 0; i < numeroGirasoli; i++)
         {
+            //scelta terrain
+            Terrain terrain = (Terrain)Instantiate(terrains[Random.Range(0, terrains.Length)]);
+            // terrain size x 
+            terrainWidth = (int)terrain.terrainData.size.x;
+            // terrain size z 
+            terrainLength = (int)terrain.terrainData.size.z;
+            // terrain x position 
+            terrainPosX = (int)terrain.transform.position.x;
+            // terrain z position 
+            terrainPosZ = (int)terrain.transform.position.z;
+
+            //posizione, rotazione e scalatura girasole
             int posx = Random.Range(terrainPosX, terrainPosX + terrainWidth);
             int posz = Random.Range(terrainPosZ, terrainPosZ + terrainWidth);
             float posy = Terrain.activeTerrain.SampleHeight(new Vector3(posx, 0, posz));
