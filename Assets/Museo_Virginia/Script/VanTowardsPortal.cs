f1bf14e3909bb005d94fbd709a15133f12debbd4
virginiamazzei <virgymazzei@gmail.com>
2022-02-10 17:18:39 +0100

    video screen

---
+++ Assets/Museo_Virginia/Script/VanTowardsPortal.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+using UnityEngine.Video;
+
+public class VanTowardsPortal : MonoBehaviour
+{
+    public GameObject _vanCamera;
+    public GameObject _mainCamera;
+    public Animator _animatorVan;
+   
+    private void Start()
+    {
+        _mainCamera.gameObject.active = true;
+        _vanCamera.gameObject.active = false;
+        _animatorVan = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // rileva il tasto W e sposta in avanti
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            cambioCamera();
+            MoveVan();
+            delayAsync();
+            
+        }
+    }
+
+    //Funzione che cambia camera
+    public void cambioCamera()
+    {
+        if (_mainCamera.gameObject.active) //Guida VAN
+        { 
+            _mainCamera.gameObject.active = false;
+            _vanCamera.gameObject.active = true;
+
+        }
+    }
+
+    public void MoveVan()
+    {
+        if (_animatorVan == null)
+            return;
+
+      _animatorVan.SetBool("move", true);
+
+    }
+
+    public async Task delayAsync() //Ritardo la scomparsa dell'orecchio cos? l'utente pu? vedere cosa prende
+                                   //poi scompare cos? non deve tenerlo in mano, "come se se lo mettesse"
+    {
+        await Task.Delay(3000);
+        SceneManager.LoadScene("Mondo");
+    }
+}
