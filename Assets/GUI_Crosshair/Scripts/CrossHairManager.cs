98e4530c79b5a417afbe2bcecd80168691bc754d
Roberta Macaluso <roby_97@outlook.it>
2022-02-20 17:48:59 +0100

    Fix cursore per Interactable

--- Assets/GUI_Crosshair/Scripts/CrossHairManager.cs
+++ Assets/GUI_Crosshair/Scripts/CrossHairManager.cs
@@ -8,20 +8,28 @@ public class CrossHairManager : MonoBehaviour
     [SerializeField] private Transform _fpsCameraT;
     [SerializeField] CharacterController _fpsController;
     [SerializeField] private float _interactionDistance;
+
+    [Header("Objects")]
+    [SerializeField] private GameObject _crossHairDefault;
     [SerializeField] Image[] Images;
+    [SerializeField] private GameObject _crossHairSelect;
+    [SerializeField] Image ImageSelect;
+    [SerializeField] Image[] ImagesSelect;
 
     private Vector3 _rayOrigin;
     private Interactable _pointingInteractable;
     private Grabbable _pointingGrabbable;
 
-    // Update is called once per frame
-    void Update()
-    {
-        gameObject.SetActive(true);
+    void Start(){
+        _crossHairSelect.SetActive(false);
+        _crossHairDefault.SetActive(false);
         foreach(Image i in Images){
             i.color = Color.white;
         }
-        
+    }
+
+    void Update()
+    {       
         _rayOrigin = _fpsCameraT.position + _fpsController.radius/4 * _fpsCameraT.forward;
 
         Ray ray = new Ray(_rayOrigin, _fpsCameraT.forward);
@@ -34,19 +42,33 @@ public class CrossHairManager : MonoBehaviour
 
             //Check if is interactable
             if (_pointingInteractable) {
-                foreach(Image i in Images){
+                _crossHairDefault.SetActive(false);
+                _crossHairSelect.SetActive(true);
+                ImageSelect.color = Color.red;
+                foreach(Image i in ImagesSelect){
                     i.color = Color.red;
                 }
             }           
             else if (_pointingGrabbable){
+                _crossHairSelect.SetActive(false);
+                _crossHairDefault.SetActive(true);
                 foreach(Image i in Images){
                     i.color = Color.blue;
                 }
             }
             else {
+                _crossHairSelect.SetActive(false);
+                _crossHairDefault.SetActive(false);
                 foreach(Image i in Images){
                     i.color = Color.white;
                 }
+            }
+        }
+        else {
+            _crossHairSelect.SetActive(false);
+            _crossHairDefault.SetActive(false);
+            foreach(Image i in Images){
+                i.color = Color.white;
             }
         }
     }
