353e09506d26aa8c105d7d4b9b13b3f46a76d41b
Roberta Macaluso <roby_97@outlook.it>
2022-02-21 21:10:00 +0100

    Implementazione Apparizione Oggetti dello zaino nel museo (al ritorno))

---
+++ Assets/ShowObjects.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class ShowObjects : MonoBehaviour
+{
+    public List<GameObject> Oggetti = new List<GameObject>();
+
+    private string path = "ZainoObjs.txt";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> Objs = LeggiFile();
+        foreach(GameObject obj in Oggetti){
+            if (Objs.Contains(obj.name))
+                obj.SetActive(true);
+        }
+    }
+
+    private List<string> LeggiFile(){
+        //Lettura da file
+        List<string> Objs = new List<string>();;
+        string s;
+
+        StreamReader sr = File.OpenText(path);
+        while ((s = sr.ReadLine()) != null){
+            Objs.Add(s);
+        }
+        sr.Close();
+        return Objs;
+    }
+}
